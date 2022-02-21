aee1c6e92a5d615cd501a757ba8fccdd467e883a
Benjamin Vinterberg <benj924c@stud.kea.dk>
2022-02-21 10:29:15 +0100

    Tilføjet knapper med data (fungerer ikke)

--- script.js
+++ script.js
@@ -7,25 +7,44 @@ const options = {
   },
 };
 
+let kategorier;
+let filter = "alle";
+
+start = () => {
+  const filterKnapper = document.querySelectorAll("nav button");
+  filterKnapper.forEach((knap) =>
+    knap.addEventListener("click", filtrerKategorier)
+  );
+  hentData();
+};
+
+const filtrerKategorier = () => {
+  filter = this.dataset.kategori;
+  console.log(filter);
+  hentData();
+};
+
 async function hentData() {
   const resspons = await fetch(api, options);
-  const kategori = await resspons.json();
-  formatering(kategori);
+  kategorier = await resspons.json();
+  formatering(kategorier);
 }
 
-const formatering = (kategori) => {
+const formatering = (kategorier) => {
   const main = document.querySelector("#content");
   const template = document.querySelector("template").content;
-  kategori.forEach((kategori) => {
-    const klon = template.cloneNode(true);
-    klon.querySelector("h2").textContent = kategori.navn;
-    klon.querySelector(".beskrivelse").textContent = kategori.beskrivelse;
-    klon.querySelector(".billede").src = `./img/${kategori.billede}`;
-    klon.querySelector(".billede").alt = kategori.alttag;
-    main.appendChild(klon);
+  kategorier.forEach((kategorier) => {
+    if (filter == kategorier.kategori || filter == "alle") {
+      const klon = template.cloneNode(true);
+      klon.querySelector("h2").textContent = kategorier.navn;
+      klon.querySelector(".beskrivelse").textContent = kategorier.beskrivelse;
+      klon.querySelector(".billede").src = `./img/${kategorier.billede}`;
+      klon.querySelector(".billede").alt = kategorier.alttag;
+      main.appendChild(klon);
+    }
   });
-  console.log(kategori);
+  console.log(kategorier);
 };
 
 console.log("wtf");
-hentData();
+start();
